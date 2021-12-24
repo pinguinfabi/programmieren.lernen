184871de00ea86e5ed821232f675e375fa06892e
likl <84447358+likl@users.noreply.github.com>
2021-12-24 08:01:37 +0100

    Update login.js

--- login/login.js
+++ login/login.js
@@ -7,9 +7,8 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
+    if (username === "likl" && password === "likl") {
+        window.open("../index.html","_self")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
